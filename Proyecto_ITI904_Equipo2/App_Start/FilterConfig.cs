a65b31199d9378b98a253af3ba1d3e81361bc904
Angel Gutierrez <angel.g.hernandez.1997@gmail.com>
2020-07-06 13:53:25 -0500

    Initial commit

---
+++ Proyecto_ITI904_Equipo2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_ITI904_Equipo2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Proyecto_ITI904_Equipo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_ITI904_Equipo2.MvcApplication" Language="C#" %>
