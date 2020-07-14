7aadfa52c9932533bb46b1ec76ade60465546a42
Soonnk <angel.g.hernandez.1997@gmail.com>
2020-07-14 00:30:54 -0500

    User Authentication Update
    "Quién necesita el pinche scaffolder culero >:v" -yo (2020)
    
    Agregada la autenticación de usuarios (falta vincular y personalizar las vistas)

---
+++ Proyecto_ITI904_Equipo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+
+namespace Proyecto_ITI904_Equipo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
