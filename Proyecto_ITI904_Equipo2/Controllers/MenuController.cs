661201922479352d8f414127ad4a5050711ba530
Soonnk <angel.g.hernandez.1997@gmail.com>
2020-08-14 17:23:17 -0500

    Navegación y ajustes visuales
    Se agregó un menú segun san Raynold y se corrigió el bug visual del carrito, se agregan algunos iconos especializados.
    
    Las recetas ahora se renderizan en la sección de Menú

--- Proyecto_ITI904_Equipo2/Controllers/MenuController.cs
+++ Proyecto_ITI904_Equipo2/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_ITI904_Equipo2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace Proyecto_ITI904_Equipo2.Controllers
 {
     public class MenuController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Menu
         public ActionResult Index()
         {
-            return View();
+            return View(db.Recetas.ToList());
         }
     }
 }
